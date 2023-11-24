a42968b5d53d41910746f15c5feb1cd8c9fca80b
Bruno Rafael <brunrb96@gmail.com>
2023-11-24 17:43:15 -0300

    Desafio 01 TS - 24_11_2023 - 17:43

--- class/CompanyAccount.ts
+++ class/CompanyAccount.ts
@@ -1,12 +1,12 @@
-import { DioAccount } from "./DioAccount"
+import { DioAccount } from "./DioAccount";
 
 export class CompanyAccount extends DioAccount {
 
   constructor(name: string, accountNumber: number){
-    super(name, accountNumber)
+    super(name, accountNumber);
   }
 
   getLoan = (): void => {
-    console.log('Voce pegou um empréstimo')
+    console.log('Voce pegou um empréstimo');
   }
 }
--- class/DioAccount.ts
+++ class/DioAccount.ts
@@ -1,42 +1,41 @@
 export abstract class DioAccount {
-  private name: string
-  private readonly accountNumber: number
-  balance: number = 0
-  private status: boolean = true
+  private readonly name: string;
+  private readonly accountNumber: number;
+  balance: number = 0;
+  private status: boolean = true;
 
   constructor(name: string, accountNumber: number){
-    this.name = name
-    this.accountNumber = accountNumber
+    this.name = name;
+    this.accountNumber = accountNumber;
   }
 
-  setName = (name: string): void => {
-    this.name = name
-    console.log('Nome alterado com sucesso!')
+  setName = (): void => {
+    console.log("Náo é possível alterar o nome!");
   }
 
   getName = (): string => {
-    return this.name
+    return this.name;
   }
 
   deposit = (): void => {
     if(this.validateStatus()){
-      console.log('Voce depositou')
+      console.log('Voce depositou');
     }
   }
 
   withdraw = (): void => {
-    console.log('Voce sacou')
+    console.log('Voce sacou');
   }
 
   getBalance = (): void => {
-    console.log(this.balance)
+    console.log(this.balance);
   }
 
   private validateStatus = (): boolean => {
     if (this.status) {
-      return this.status
+      return this.status;
     }
 
-    throw new Error('Conta inválida')
+    throw new Error('Conta inválida');
   }
 }
